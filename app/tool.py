07fb41dbc28b8cdb1673f9fa19fe4cf5b758e802
DESKTOP-CSBAJF8\WLOPEZOB <wlopezob@gmail.com>
2025-02-21 15:30:37 -0500

    feat

--- app/tool.py
+++ app/tool.py
@@ -1,21 +1,22 @@
+from langgraph.graph import MessagesState
+from langgraph.graph import StateGraph, START, END
 from langchain_openai import ChatOpenAI
-from langgraph.graph import StateGraph, START, END, MessagesState
 from langchain_core.messages import SystemMessage
-from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.prebuilt import ToolNode
+from langgraph.prebuilt import tools_condition
+import requests
 
 class State(MessagesState):
     my_var: str
 
-def multiply(a: int, b:int) -> int:
-    """Multitply a and b.
 
-    Args:
-        a: first int
-        b: second int
-    """
+# Define the tools
+
+def multiply(a: int, b: int) -> int:
+    """Multiplies a and b."""
     return a * b
 
-def add(a: int, b:int) -> int:
+def add(a: int, b: int) -> int:
     """Adds a and b.
 
     Args:
@@ -24,26 +25,39 @@ def add(a: int, b:int) -> int:
     """
     return a + b
 
-tools = [multiply, add]
-llm = ChatOpenAI(model="gpt-4o", temperature=0.5, max_tokens=2000)
-lm = llm.bind_tools(tools, parallel_tool_calls=False)
+
+def get_categories() -> str:
+    """ Get the categories of the products."""
+
+    response = requests.get("https://api.escuelajs.co/api/v1/categories")
+    categories = response.json()
+    category_names = [category["name"] for category in categories]
+    return ", ".join(category_names)
+
+
+tools = [multiply, add, get_categories]
+
+# Define the nodes  
+
+llm = ChatOpenAI(model="gpt-4o", temperature=0)
+llm = llm.bind_tools(tools, parallel_tool_calls=False)
 
 
 def assistant(state: State) -> State:
-    system_message = SystemMessage(content="Eres un experto en matematicas y debes de ayudar a resolver problemas")
-    message = llm.invoke([system_message] + state["messages"])
-    return {"messages": message}
+    system_message = SystemMessage(content="Eres un experto en matemáticas y además tienes acceso a las categorías de los productos.")
+    
+    return {"messages": [llm.invoke([system_message] + state["messages"])]}
+
+
+# Define the graph
 
 builder = StateGraph(State)
 
 builder.add_node('assistant', assistant)
 builder.add_node("tools", ToolNode(tools))
 
-
 builder.add_edge(START, 'assistant')
-builder.add_conditional_edges('assistant', 
-                              # evalua si la respuesa del mensaje tiene contenido o si el resultado sera la ejecucion de una funcion
-                              tools_condition)
-builder.add_edge('tools', 'assistant')
+builder.add_conditional_edges("assistant", tools_condition)
+builder.add_edge("tools", "assistant")
 
 graph = builder.compile()
